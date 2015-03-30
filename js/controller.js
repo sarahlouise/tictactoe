0d0a400277aca3430f9258c6946c07f62bcda8c7
Sarah Kruberg <sarah.kruberg@gmail.com>
2015-03-30 12:05:50 -0700

    all three pages edited for accutate html and css - have an issue with text now not being at the center of buttons, rather it's at the top

---
+++ js/controller.js
@@ -0,0 +1,25 @@
+angular
+	.module('TicTacToe')
+	.controller("TicTacToe", TicTacToe);
+
+	function TicTacToe(){
+		var self = this;
+
+
+		for(i=0;i<9;i++){
+			[{space: ''}];
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+	} ///////CLOSES TICTACTOE CONTROLLER//////////
