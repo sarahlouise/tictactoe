a6ed8b69df1d42fe905e0ece9fb1bbf62358b932
Sarah Kruberg <sarah.kruberg@gmail.com>
2015-04-06 00:41:21 -0700

    working firebase with features from local project

--- js/app.js
+++ js/app.js
@@ -1,2 +1,2 @@
 angular
-    .module('TicTacToeApp', [])
+    .module('App', ['firebase']); 
--- js/controller.js
+++ js/controller.js
@@ -1,178 +1,190 @@
 angular
-	.module('TicTacToeApp')
-	.controller('TicTacToeController', TicTacToeController)
+    .module('App')
+    .controller('TicTacToeController', TicTacToeController);
 
-	//Firebase//
-	// .controller("TicTacToeController", function($scope, $firebaseObject) {
- //  	var ref = new Firebase("https://sarahstictactoe.firebaseio.com/");
-  	
-  	// download the data into a local object
-	// TicTacSyncObj = $firebaseObject(ref);
+    TicTacToeController.$inject = ['$firebaseObject'];
 
-    // synchronize the object with a three-way data binding
- // 	 TicTacSyncObj.$bindTo($scope, "TicTacSyncObj");
-	
-	// });
+    function TicTacToeController($firebaseObject){
+        var self = this;  
 
-	///Firebase initiation end//
+        self.game = syncGameWithFirebase();
+        self.playerClick = playerClick; //sets playerClick() function as a property of the controller
+        self.tieTally = tieTally;
+        //begin and end popups and functions
+        self.beginPlay = beginPlay;
+        self.getWinner = getWinner;
+        self.clearboard = clearboard; //resets spaces for game
+        self.gameOver = gameOver;
 
-	function TicTacToeController(){ 
-		var self = this;
-		// var ref = new Firebase('https://sarahstictactoe.firebaseio.com')
-		self.username; //Stores input from mainpage username;
-		self.turn = 1;
-		self.playerXscore = 0; //ng-model to gameboard
-		self.playerOscore = 0; //ng-model to gameboard
+        self.submit = false; //tied to login button and beingPlay function
+        
+        function syncGameWithFirebase(){
+            var ref = new Firebase('https://sarahstictactoe.firebaseio.com');
+            var gameObject = $firebaseObject(ref);
 
-		self.tieGame = 0;
-		self.occupierTally = 0;
-		self.tieTally = tieTally;
+            //initialize values in the gameObject once it's loaded
+            gameObject.$loaded(function(){
+                gameObject.turn = 1;
+                gameObject.playerXscore = 0; //ng-model to gameboard
+                gameObject.playerOscore = 0; //ng-model to gameboard
+                gameObject.tieGame = 0;
+                gameObject.occupierTally = 0;
+                gameObject.username; //Stores input from mainpage username;
+                gameObject.playAgain = false;  //false = gameover sceen is hidden, tied to 
+                gameObject.spaces = [];
+                for(var i = 0; i < 9; i++){
+                    gameObject.spaces.push({occupier: '', playerX: false, playerO: false});
+                }
 
-		self.playerClick = playerClick;
-		self.beginPlay = beginPlay;
+                gameObject.$save();
+            });
 
-		self.getWinner = getWinner;
-		self.clearboard = clearboard; //resets spaces for game
+            return gameObject;
+        }
 
-		self.submit = false; //tied to login button and beingPlay function
-		self.playAgain = false;  //false = gameover sceen is hidden, tied to 
-		self.gameOver = gameOver;
+        //called using ng-click directive in the view
+        function playerClick($index){
+            console.log('playerClick Initiated')
+            if (self.game.spaces[$index].occupier === 'X'){
+                console.log("no no, this space is taken");
+            } else if(self.game.spaces[$index].occupier === 'O'){
+                console.log("no no, this space is taken");
+            } else if(self.game.turn === 1) {
+                self.game.spaces[$index].occupier = 'X', self.game.occupierTally++, self.game.spaces[$index].playerX = true, self.game.turn = 2; self.game.$save();
+            } else if(self.game.turn === 2){
+                self.game.spaces[$index].occupier = 'O', self.game.occupierTally++, self.game.spaces[$index].playerO = true, self.game.turn = 1; self.game.$save();
+                };
+            tieTally();
+            getWinner();
 
-		self.spaces = [
-			{occupier: '', playerX: false, playerO: false},
-			{occupier: '', playerX: false, playerO: false},
-			{occupier: '', playerX: false, playerO: false},
-			{occupier: '', playerX: false, playerO: false},
-			{occupier: '', playerX: false, playerO: false},
-			{occupier: '', playerX: false, playerO: false},
-			{occupier: '', playerX: false, playerO: false},
-			{occupier: '', playerX: false, playerO: false},
-			{occupier: '', playerX: false, playerO: false}
-				]; 
+            }; ///END PLAYER CLICK FUNCTION
 
-		function playerClick($index){
-			if (self.spaces[$index].occupier === 'X'){
-				console.log("no no, this space is taken");
-			} else if(self.spaces[$index].occupier === 'O'){
-				console.log("no no, this space is taken");
-			} else if(self.turn === 1) {
-				self.spaces[$index].occupier = 'X', self.occupierTally++, self.spaces[$index].playerX = true, self.turn = 2;
-			} else if(self.turn === 2){
-				self.spaces[$index].occupier = 'O', self.occupierTally++, self.spaces[$index].playerO = true, self.turn = 1;
-				};
-			tieTally();
-			getWinner();
+  //BEGIN NEW GAME
+        function beginPlay(){
+            self.submit = true};
 
-			}; ///END PLAYER CLICK FUNCTION
+        function gameOver(){
+            console.log('gameOver initiated');
+            self.game.playAgain = false;
+            self.game.$save();
+            };//hides gameover screen
 
+        function tieTally(){
+            console.log(self.game.occupierTally);
 
-		//BEGIN NEW GAME
-		function beginPlay(){
-			self.submit = true};
+            if(self.game.occupierTally == 9){
+                console.log("It's a tie!");
+                self.game.tieGame++;
+                self.game.$save();
+                };
+            }; //closes TieTally
 
-		function gameOver(){
-			console.log('gameOver initiated');
-			return self.playAgain = false;};//hides gameover screen
+        //BEGIN GET WINNER FUNCTION
+        function getWinner(){
+            var row1 =[self.game.spaces[0].occupier + self.game.spaces[1].occupier + self.game.spaces[2].occupier];
+            var row2 = [self.game.spaces[3].occupier + self.game.spaces[4].occupier + self.game.spaces[5].occupier];
+            var row3 = [self.game.spaces[6].occupier + self.game.spaces[7].occupier + self.game.spaces[8].occupier];
+            var diag1 = [self.game.spaces[6].occupier + self.game.spaces[4].occupier + self.game.spaces[2].occupier];
+            var diag2 = [self.game.spaces[0].occupier + self.game.spaces[4].occupier + self.game.spaces[8].occupier];
+            var column1 = [self.game.spaces[0].occupier + self.game.spaces[3].occupier + self.game.spaces[6].occupier];
+            var column2 = [self.game.spaces[1].occupier + self.game.spaces[4].occupier + self.game.spaces[7].occupier];
+            var column3 = [self.game.spaces[2].occupier + self.game.spaces[5].occupier + self.game.spaces[8].occupier]
 
-		function tieTally(){
-			console.log(self.occupierTally);
+            if(row1 == 'XXX'){
+                console.log("X won!"); 
+                self.game.playerXscore ++; 
+                self.game.$save();
+            }else if (row2 == 'XXX'){
+                console.log("X won!"); 
+                self.game.playerXscore ++; 
+                self.game.$save();
+            }else if (row3 == 'XXX'){
+                console.log("X won!"); 
+                self.game.playerXscore ++; 
+                self.game.$save();
+            }else if (diag1 == 'XXX'){
+                console.log("X won!"); 
+                self.game.playerXscore ++;
+                self.game.$save();
+            }else if (diag2 == 'XXX'){
+                console.log("X won!"); 
+                self.game.playerXscore ++;
+                self.game.$save();
+            }else if (column1 == 'XXX'){
+                console.log("X won!"); 
+                self.game.playerXscore ++;
+                self.game.$save();
+            }else if (column2 == 'XXX'){
+                console.log("X won!"); 
+                self.game.playerXscore ++;
+                self.game.$save();
+            }else if (column3 == 'XXX'){
+                console.log("X won!"); 
+                self.game.playerXscore ++;
+                self.game.$save();
+            //assess O win
+            }else if(row1 == 'OOO'){
+                console.log("O won!"); 
+                self.game.playerOscore ++;
+                self.game.$save();
+            }else if (row2 == 'OOO'){
+                console.log("O won!"); 
+                self.game.playerOscore ++;
+                self.game.$save();
+            }else if (row3 == 'OOO'){
+                console.log("O won!"); 
+                self.game.playerOscore ++;
+                self.game.$save();
+            }else if (diag1 == 'OOO'){
+                console.log("O won!"); 
+                self.game.playerOscore ++;
+                self.game.$save();
+            }else if (diag2 == 'OOO'){
+                console.log("O won!"); 
+                self.game.playerOscore ++;
+                self.game.$save();
+            }else if (column1 == 'OOO'){
+                console.log("O won!"); 
+                self.game.playerOscore ++;
+                self.game.$save();
+            }else if (column2 == 'OOO'){
+                console.log("O won!"); 
+                self.game.playerOscore ++;
+                self.game.$save();
+            }else if (column3 == 'OOO'){
+                console.log("O won!"); 
+                self.game.playerOscore ++;
+                self.game.$save();
+            }
+        };//END GET WINNER FUNCTION//
 
-			if(self.occupierTally == 9){
-				console.log("It's a tie!");
-				self.tieGame++;
-				};
-			}; //closes TieTally
+        function clearboard(){
+            self.game.spaces = [
+            {occupier: '', playerX: false, playerO: false},
+            {occupier: '', playerX: false, playerO: false},
+            {occupier: '', playerX: false, playerO: false},
+            {occupier: '', playerX: false, playerO: false},
+            {occupier: '', playerX: false, playerO: false},
+            {occupier: '', playerX: false, playerO: false},
+            {occupier: '', playerX: false, playerO: false},
+            {occupier: '', playerX: false, playerO: false},
+            {occupier: '', playerX: false, playerO: false}
+                ];
+            console.log('board clearboard');
+            self.game.occupierTally = 0;
+            self.game.$save();
 
-		//BEGIN GET WINNER FUNCTION
-		function getWinner(){
-			var row1 =[self.spaces[0].occupier + self.spaces[1].occupier + self.spaces[2].occupier];
-			var row2 = [self.spaces[3].occupier + self.spaces[4].occupier + self.spaces[5].occupier];
-			var row3 = [self.spaces[6].occupier + self.spaces[7].occupier + self.spaces[8].occupier];
-			var diag1 = [self.spaces[6].occupier + self.spaces[4].occupier + self.spaces[2].occupier];
-			var diag2 = [self.spaces[0].occupier + self.spaces[4].occupier + self.spaces[8].occupier];
-			var column1 = [self.spaces[0].occupier + self.spaces[3].occupier + self.spaces[6].occupier];
-			var column2 = [self.spaces[1].occupier + self.spaces[4].occupier + self.spaces[7].occupier];
-			var column3 = [self.spaces[2].occupier + self.spaces[5].occupier + self.spaces[8].occupier]
+            //Game to 5//
+            if(self.game.playerOscore + self.game.tieGame + self.game.playerXscore == 5){
+                console.log('Well Played! Game Over');
+                self.game.playerXscore = 0;
+                self.game.playerOscore = 0;
+                self.game.tieGame = 0; 
+                self.game.playAgain = true;
+                self.game.$save();
+            }
+        };
+        //End Clearboard functioon 
 
-			if(row1 == 'XXX'){
-				console.log("X won!"); 
-				self.playerXscore ++;
-			}else if (row2 == 'XXX'){
-				console.log("X won!"); 
-				self.playerXscore ++;
-			}else if (row3 == 'XXX'){
-				console.log("X won!"); 
-				self.playerXscore ++;
-			}else if (diag1 == 'XXX'){
-				console.log("X won!"); 
-				self.playerXscore ++;
-			}else if (diag2 == 'XXX'){
-				console.log("X won!"); 
-				self.playerXscore ++;
-			}else if (column1 == 'XXX'){
-				console.log("X won!"); 
-				self.playerXscore ++;
-			}else if (column2 == 'XXX'){
-				console.log("X won!"); 
-				self.playerXscore ++;
-			}else if (column3 == 'XXX'){
-				console.log("X won!"); 
-				self.playerXscore ++;
-			//assess O win
-			}else if(row1 == 'OOO'){
-				console.log("O won!"); 
-				self.playerOscore ++;
-			}else if (row2 == 'OOO'){
-				console.log("O won!"); 
-				self.playerOscore ++;
-			}else if (row3 == 'OOO'){
-				console.log("O won!"); 
-				self.playerOscore ++;
-			}else if (diag1 == 'OOO'){
-				console.log("O won!"); 
-				self.playerOscore ++;
-			}else if (diag2 == 'OOO'){
-				console.log("O won!"); 
-				self.playerOscore ++;
-			}else if (column1 == 'OOO'){
-				console.log("O won!"); 
-				self.playerOscore ++;
-			}else if (column2 == 'OOO'){
-				console.log("O won!"); 
-				self.playerOscore ++;
-			}else if (column3 == 'OOO'){
-				console.log("O won!"); 
-				self.playerOscore ++;
-			}
-		};//END GET WINNER FUNCTION//
-
-		function clearboard(){
-			
-			self.spaces = [
-			{occupier: '', playerX: false, playerO: false},
-			{occupier: '', playerX: false, playerO: false},
-			{occupier: '', playerX: false, playerO: false},
-			{occupier: '', playerX: false, playerO: false},
-			{occupier: '', playerX: false, playerO: false},
-			{occupier: '', playerX: false, playerO: false},
-			{occupier: '', playerX: false, playerO: false},
-			{occupier: '', playerX: false, playerO: false},
-			{occupier: '', playerX: false, playerO: false}
-				];
-			console.log('board clearboard');
-			self.occupierTally = 0;
-
-			//Game to 5//
-			if(self.playerOscore + self.tieGame + self.playerXscore == 5){
-				console.log('Well Played! Game Over');
-				self.playerXscore = 0;
-				self.playerOscore = 0;
-				self.tieGame = 0; 
-				gameOver = true;
-			}
-		};
-		//End Clearboard functioon 
-
-	} ///////CLOSES TICTACTOE CONTROLLER//////////
-
+    } ///////CLOSES TICTACTOE CONTROLLER//////////
 
